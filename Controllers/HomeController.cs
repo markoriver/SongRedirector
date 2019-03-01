93763901dbbcb154c3ee658fda02494cb8f548d7
Martin Thaller <thaller.martin@gmail.com>
2019-03-02 00:35:02 +0100

    default mvc app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SongRedirector.Models;
+
+namespace SongRedirector.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("https://google.com");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
